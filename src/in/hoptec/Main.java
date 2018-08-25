ba55a8b31ac65a3fc954dd5f72a07f2563142ad7
shiveshnavin <shiveshnavin@gmail.com>
2018-08-25 23:39:56 +0530

    overrirde test

--- src/in/hoptec/Main.java
+++ src/in/hoptec/Main.java
@@ -3,15 +3,40 @@ package in.hoptec;
 public class Main {
 
 
-    Main()
+    public static class Animal
     {
-        int xxs;
+        int legs=4;
+        @Override
+        public boolean equals(Object an)
+        {
+            System.out.println("overrid");
+            if(((Animal)an).legs==legs)
+            {
+                return true;
+            }
+            return false;
+        }
+
     }
 
+
+
+    public static void l(Object s)
+    {
+        System.out.println(""+s);
+    }
     public static void main(String[] args) {
-	// write your code here
-        short xx=92;
-        new Main();
+	// write your code
+
+        int x= -8;
+        int x2=0b10011;
+
+        l(" << "+(x<<1));
+
+        Animal a=new Animal();
+        l(a.equals(new Animal()));
+
+
 
     }
 }
