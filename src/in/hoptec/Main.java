ae0046a6c6149918d01cc47d47c68fe30e45ce4d
shiveshnavin <shiveshnavin@gmail.com>
2018-08-25 22:27:31 +0530

    initial commit

---
+++ src/in/hoptec/Main.java
@@ -0,0 +1,17 @@
+package in.hoptec;
+
+public class Main {
+
+
+    Main()
+    {
+        int xxs;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        short xx=92;
+        new Main();
+
+    }
+}
