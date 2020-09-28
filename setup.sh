c5e2307338d2b11848a6f66dd1e73a54eee284f3
Shivesh Navin <shiveshnavin@gmail.com>
2020-09-28 16:26:49 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-cd opt 
+cd /opt 
 curl https://nodejs.org/dist/v12.18.4/node-v12.18.4-linux-x64.tar.xz  --output node.tar.xz
 zcat node.tar.xz | tar -xvf - 
 export PATH="/opt/node-v12.18.4-linux-x64/bin:/opt/node-v12.18.4-linux-x64/lib/node_modules:$PATH" 
