c2cbe8403277310efb27272bbd422fde6393b325
Shivesh Navin <shiveshnavin@gmail.com>
2020-09-28 16:54:48 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ cd /opt
 curl https://nodejs.org/dist/v12.18.4/node-v12.18.4-linux-x64.tar.xz  --output node.tar.xz
 zcat node.tar.xz | tar -xvf - 
 export PATH="/opt/node-v12.18.4-linux-x64/bin:/opt/node-v12.18.4-linux-x64/lib/node_modules:$PATH" 
+
+git clone https://github.com/efeiefei/node-file-manager.git
+cd node-file-manager
+npm i
+cd lib
+node --harmony index.js -p 8080 -d /
